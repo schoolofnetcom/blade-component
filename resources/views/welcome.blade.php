d5ec4fa0662cf3f78e51502bdaa5b9e48180112b
thivalls <olavalls@gmail.com>
2020-12-01 15:56:19 -0300

    Aula 1 - Introdução

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,16 @@
 @section('content')
     <h1>Hello world!!!</h1>
     <p>Welcome to blade component course...</p>
+
+    {{--
+    <form action="">
+        <inputText label="meu label" type="" />
+        <component label="meu label" type="" />
+        <component label="meu label" type="" />
+        <component label="meu label" type="" />
+        <component label="meu label" type="" />
+        <component label="meu label" type="" />
+        <component label="meu label" type="" />
+    </form>
+    --}}
 @endsection
